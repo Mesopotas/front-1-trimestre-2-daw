ddcfcae1701acc6fe3fc246c21575c36b7bb6519
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-27 18:20:08 +0100

    Responsive pequeño home

--- build/js/carousel.js
+++ build/js/carousel.js
@@ -1,22 +1,26 @@
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  slidesPerView: 3, // Configuración predeterminada
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    390: { // Configuración para pantallas de 390px o menos
+      slidesPerView: 1, // Solo una diapositiva visible
+      centeredSlides: true, // Centrar la diapositiva activa
+      spaceBetween: 10, // Ajustar espacio entre diapositivas
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
+    640: {
+      slidesPerView: 1, // Una diapositiva para pantallas medianas
     },
-  });
-  
+    768: {
+      slidesPerView: 2, // Dos diapositivas para pantallas mayores a 768px
+    },
+    1024: {
+      slidesPerView: 3, // Tres diapositivas en pantallas grandes
+    },
+  },
+});
